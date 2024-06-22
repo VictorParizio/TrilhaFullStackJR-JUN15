1ac5474fc0c9c19bd4afff920eddea62f7c75601
Victor Parizio <victorparizio@hotmail.com>
2024-06-21 21:35:25 -0300

    feat: autenticação

---
+++ backend/src/middlewares/token.ts
@@ -0,0 +1,59 @@
+import { prisma } from "@/lib/prisma";
+import { checkToken } from "@/util/jwt";
+import { NextFunction, Request, Response } from "express";
+
+interface AuthenticatedRequest extends Request {
+  authenticatedUser?: string;
+}
+
+interface JwtPayload {
+  id: string;
+  exp?: number;
+  iat?: number;
+}
+
+export const validateToken = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res
+      .status(401)
+      .json({
+        message:
+          "Para ter acesso, faça login ou cadastre-se!",
+      });
+  }
+
+  try {
+    const token = authorization.split(" ")[1];
+    const { id } = checkToken(token) as JwtPayload;
+
+    const user = await prisma.user.findFirst({ where: { id } });
+
+    if (!user) {
+      return res.status(404).json({ message: "O usuário não foi encontrado" });
+    }
+
+    req.authenticatedUser = user.id;
+
+    next();
+  } catch (error: any) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        message:
+          "O Token de acesso expirou. Faça login para acessar esta funcionalidade.",
+      });
+    } else if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        message:
+          "Para acessar este recurso um token de autenticação válido deve ser enviado.",
+      });
+    } else {
+      return res.status(500).json({ mensagem: "Erro interno do Servidor" });
+    }
+  }
+};
--- backend/src/routers/userRouter.ts
+++ backend/src/routers/userRouter.ts
@@ -1,10 +1,13 @@
 import { loginUser } from "@/controllers/login.controller";
 import { registerUser } from "@/controllers/register.controller";
+import { validateToken } from "@/middlewares/token";
 import { Router } from "express";
 
 const route = Router();
 
 route.post("/register", registerUser);
 route.post("/session", loginUser);
+
+route.use(validateToken);
 
 export default route;
