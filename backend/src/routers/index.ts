d10bdb0328fa9db7cc2f6001547d62efe46fa657
Victor Parizio <victorparizio@hotmail.com>
2024-06-26 22:18:01 -0300

    fix: permissões de autorização

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -2,13 +2,11 @@ import express from "express";
 import userRouter from "./user.router";
 import projectRouter from "./project.router";
 import swagger from "./swagger.router";
-import { validateToken } from "@/middlewares/token";
 import { authorized } from "@/middlewares/authorized";
 
 export const router = express();
 
 router.use(swagger);
 router.use(userRouter);
-router.use(validateToken);
 router.use(authorized)
 router.use(projectRouter);
--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,5 +1,6 @@
 import { loginUser } from "@/controllers/users/login.controller";
 import { registerUser } from "@/controllers/users/register.controller";
+import { validateToken } from "@/middlewares/token";
 import { validateBody } from "@/middlewares/validate.body";
 import { userAuthSchema, userRegisterSchema } from "@/schemas/user.schema";
 import { Router } from "express";
@@ -8,5 +9,6 @@ const route = Router();
 
 route.post("/register", validateBody(userRegisterSchema), registerUser);
 route.post("/session", validateBody(userAuthSchema), loginUser);
+route.use(validateToken);
 
 export default route;
