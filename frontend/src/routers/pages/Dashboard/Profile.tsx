b48e9ea99dd650c1b0b960fcb6a0b38b2a603934
Victor Parizio <victorparizio@hotmail.com>
2024-06-29 21:55:26 -0300

    fix: descartando arquivos corrompidos

--- frontend/src/routers/pages/Dashboard/Profile.tsx
+++
@@ -1,8 +0,0 @@
-export const Profile = () => {
-  return (
-    <section className="welcome-message container-profile">
-      <h1><strong>Perfil</strong></h1>
-      <p>Esta página ainda está em construção, mas você já pode aproveitar e dar início aos seus projetos!</p>
-    </section>
-  );
-};
