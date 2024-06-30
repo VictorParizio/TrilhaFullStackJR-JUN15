ff6b009cdbaf69c1afd18c0883b967038b6da592
Victor Parizio <victorparizio@hotmail.com>
2024-06-29 21:56:46 -0300

    fix: restaurando frontend

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,44 @@
+import { FaLinkedin, FaGithub, FaUserTie } from "react-icons/fa";
+
+export const Footer = () => {
+  return (
+    <div className="container-footer">
+      <footer>
+        <ul className="links-utils">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/victorpariziobackend/"
+              target="_blank"
+            >
+              <FaLinkedin />
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://github.com/VictorParizio"
+              target="_blank"
+            >
+              <FaGithub />
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://portfolio-victor-parizio.vercel.app/"
+              target="_blank"
+            >
+              <FaUserTie />
+            </a>
+          </li>
+        </ul>
+
+        <p>
+          Desenvolvido por <strong>Victor Parizio</strong>
+        </p>
+
+        <div className="top"></div>
+      </footer>
+    </div>
+  );
+};
