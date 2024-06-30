7b2a7eb3f8be3c4b00c622171919df0a64e4c840
Victor Parizio <victorparizio@hotmail.com>
2024-06-29 22:03:24 -0300

    fix: removendo arquivos corrompidos

--- src/routers/pages/Dashboard/Contact.tsx
+++
@@ -1,8 +0,0 @@
-export const Contact = () => {
-  return (
-    <section className="welcome-message container-contact">
-      <h1><strong>Contatos</strong></h1>
-      <p>Esta página ainda está em construção, mas você já pode aproveitar e dar início aos seus projetos!</p>
-    </section>
-  );
-};
--- src/routers/pages/Dashboard/TeamMember.tsx
+++
@@ -1,8 +0,0 @@
-export const TeamMember = () => {
-  return (
-    <section className="welcome-message container-team-member">
-      <h1><strong>Membros</strong></h1>
-      <p>Esta página ainda está em construção, mas você já pode aproveitar e dar início aos seus projetos!</p>
-    </section>
-  );
-};
