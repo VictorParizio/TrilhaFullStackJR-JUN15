7f5d9b5d373151cd9bac52dec60ce8cfcfdc2692
Victor Parizio <victorparizio@hotmail.com>
2024-06-29 22:09:00 -0300

    fix: removendo arquivos excedentes

--- backend/src/util/encrypt.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-export const encryptPassword = (password: string) => {
-  return bcrypt.hash(password, 10);
-};
-
-export const comparePassword = (password: string, hash: string) => {
-  return bcrypt.compare(password, hash);
-};
